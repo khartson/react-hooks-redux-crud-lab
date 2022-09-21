c071e7d4a1bb6b98df23a094624a0d8628ace0b7
Kyle <khartson2017@gmail.com>
2022-09-21 15:29:51 -0400

    completed assignment

--- src/features/restaurants/RestaurantInput.js
+++ src/features/restaurants/RestaurantInput.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
-function RestaurantInput() {
-  return <div>Restaurant Input</div>;
+function RestaurantInput({ onRestaurantSubmit }) {
+  const [name, setName] = useState("");
+  
+  function handleInputChange(event){
+    setName(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onRestaurantSubmit(name);
+    setName('');
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Name
+          <input 
+            type='text'
+            name='name'
+            value={name}
+            onChange={handleInputChange}
+            />
+        </label>
+        <button type='submit'>Add Restaurant</button>
+      </form>
+
+    </div>)
+  ;
 }
 
 export default RestaurantInput;
--- src/features/restaurants/RestaurantsContainer.js
+++ src/features/restaurants/RestaurantsContainer.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantInput from "./RestaurantInput";
 import Restaurants from "./Restaurants";
+import { restaurantAdded } from './restaurantsSlice'
 
 function RestaurantsContainer() {
+
+  const restaurants = useSelector((state) => state.restaurants.entities);
+  const dispatch = useDispatch();
+
+  function handleRestaurantSubmit(name) {
+    dispatch(restaurantAdded(name));
+  }
+
+
   return (
     <div>
-      <RestaurantInput />
-      <Restaurants />
+      <RestaurantInput onRestaurantSubmit={handleRestaurantSubmit}/>
+      <Restaurants restaurants={restaurants} />
     </div>
   );
 }
